ab4f85c4807a5f5d00a9ffb2c1203621b8acfdbc
tomat <xxlennygamerxx@gmail.com>
2023-12-10 01:27:47 -0800

    This very well may be the worst code I have ever had the displeasure of writing.

--- Core/Features/UI/MainMenuRenderer.cs
+++ Core/Features/UI/MainMenuRenderer.cs
@@ -1,7 +1,12 @@
 ﻿using JetBrains.Annotations;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Graphics;
+using System;
 using Terraria;
+using Terraria.GameContent;
 using Terraria.ModLoader;
+using Terraria.UI.Chat;
 
 namespace Nitrate.Core.Features.UI;
 
@@ -19,6 +24,22 @@ internal sealed class MainMenuRenderer : ModSystem
     {
         orig(menuColor, upBump);
 
-        ModContent.GetInstance<BoxRenderer>().DrawBox(Main.spriteBatch, new Rectangle(12, 12, 50, 50));
+        const int padding = 14;
+
+        DynamicSpriteFont font = FontAssets.MouseText.Value;
+        float charHeight = font.MeasureString("A").Y;
+
+        const float title_version_spacing = 8f;
+        const float small_text_scale = 0.75f;
+        const string nitrate_title = "Nitrate";
+        string nitrateVersion = $"v{ModContent.GetInstance<NitrateMod>().Version} :)";
+
+        ModContent.GetInstance<BoxRenderer>().DrawBox(Main.spriteBatch, new Rectangle(padding, padding, 425, (int)(charHeight + charHeight * small_text_scale * 3)));
+
+        ChatManager.DrawColorCodedStringWithShadow(Main.spriteBatch, font, nitrate_title, new Vector2(padding, padding), Color.White, 0f, Vector2.Zero, Vector2.One);
+        ChatManager.DrawColorCodedStringWithShadow(Main.spriteBatch, font, nitrateVersion, new Vector2(padding + font.MeasureString(nitrate_title).X + title_version_spacing, padding + charHeight * ((1f - small_text_scale) / 2f)), Color.White, 0f, Vector2.Zero, new Vector2(small_text_scale));
+
+        ChatManager.DrawColorCodedStringWithShadow(Main.spriteBatch, font, "Please bear in mind that Nitrate is still under active development.\nFeatures may be broken, tweaked, or removed/reworked entirely.\nPlease report any issues and keep in mind that your game may break!",
+            new Vector2(padding, padding + charHeight), Color.PaleVioletRed, 0f, Vector2.Zero, new Vector2(small_text_scale));
     }
 }
