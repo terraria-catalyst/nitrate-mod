beb75d5ef190669413e58409646bd936a2a0e13b
tomat <xxlennygamerxx@gmail.com>
2023-12-17 18:36:52 -0800

    Show In-World Warning About Tile Renderer Instability (#4)

---
+++ Core/Utilities/LocalizationUtil.cs
@@ -0,0 +1,10 @@
+﻿using Terraria.Localization;
+
+namespace Nitrate.Core.Utilities;
+
+internal static class LocalizationUtil
+{
+    public static string Localize(this string key, params object?[] args) => Language.GetTextValue(key, args);
+
+    public static string LocalizeNitrate(this string key, params object?[] args) => Language.GetTextValue($"Mods.Nitrate.{key}", args);
+}
