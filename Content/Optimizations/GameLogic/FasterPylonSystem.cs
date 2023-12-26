7a2784b4ab3bec62fa221bd9d1bfaf18137d855c
Golfing7 <51512085+Golfing7@users.noreply.github.com>
2023-12-26 17:12:57 -0500

    Optimize pylon network (#13)
    Co-authored-by: tomat <xxlennygamerxx@gmail.com>

---
+++ Content/Optimizations/GameLogic/FasterPylonSystem.cs
@@ -0,0 +1,86 @@
+﻿using JetBrains.Annotations;
+using Microsoft.Xna.Framework;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ModLoader;
+
+namespace Nitrate.Content.Optimizations.GameLogic;
+
+/// <summary>
+///     Optimizes internal code for pylons.
+/// </summary>
+[UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
+public class FasterPylonSystem : ModSystem
+{
+    public override void OnModLoad()
+    {
+        base.OnModLoad();
+
+        IL_TeleportPylonsSystem.IsPlayerNearAPylon += SpeedUpIsPlayerNearAPylon;
+    }
+
+    /// <summary>
+    ///     The vanilla implementation of
+    ///     <see cref="TeleportPylonsSystem.IsPlayerNearAPylon"/> uses the
+    ///     <see cref="Player.IsTileTypeInInteractionRange"/> method to find if
+    ///     any pylon is nearby the player.
+    ///     <br />
+    ///     <br />
+    ///     The problem:
+    ///     <br />
+    ///     A single invocation of this method can (and usually does) perform
+    ///     over 14,000 tile lookups.
+    ///     <br />
+    ///     <br />
+    ///     The solution:
+    ///     <br />
+    ///     Instead of searching for nearby pylons from the player, search for
+    ///     the player from every pylon. The game limits the amount of pylons
+    ///     that can be placed, putting an upper bound on the potential downside
+    ///     of this operation.
+    ///     <br />
+    ///     <br />
+    ///     Results:
+    ///     <br />
+    ///     From testing, the original method would perform at an average of
+    ///     0.5ms per invocation.
+    ///     <br />
+    ///     The updated method reduces this to 0.005ms per invocation on
+    ///     average.
+    /// </summary>
+    private static void SpeedUpIsPlayerNearAPylon(ILContext il)
+    {
+        ILCursor cursor = new(il);
+        cursor.Emit(OpCodes.Ldarg_0);
+
+        cursor.EmitDelegate<Func<Player, bool>>(player =>
+        {
+            foreach (TeleportPylonInfo info in Main.PylonSystem.Pylons)
+            {
+                Point16 pos = info.PositionInTiles;
+                Point16 lowerRightPylonPoint = new(pos.X + 2, pos.Y + 3);
+
+                Point playerPos = player.position.ToTileCoordinates();
+
+                TileReachCheckSettings.Pylons.GetRanges(player, out int x, out int y);
+                int minRangeX = Utils.Clamp(pos.X - x, 0, Main.maxTilesX - 1);
+                int maxRangeX = Utils.Clamp(lowerRightPylonPoint.X + x - 1, 0, Main.maxTilesX - 1);
+                int minRangeY = Utils.Clamp(pos.Y - y - 1, 0, Main.maxTilesY - 1);
+                int maxRangeY = Utils.Clamp(lowerRightPylonPoint.Y + y - 1, 0, Main.maxTilesY - 1);
+
+                if (playerPos.X >= minRangeX && playerPos.X <= maxRangeX && playerPos.Y >= minRangeY && playerPos.Y <= maxRangeY)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        });
+
+        cursor.Emit(OpCodes.Ret);
+    }
+}
---
+++ Content/Optimizations/GameLogic/README.md
@@ -0,0 +1,31 @@
+﻿# Optimizations: Game Logic
+
+Contains optimizations pertaining to generic game logic (specifically logic that does not belong to existing, established areas: tile rendering, parallelization, particle rendering, etc.).
+
+## `FasterPylonSystem`
+
+> Authored by:
+> - [@Golfing7](https://github.com/Golfing7)
+>
+> Implemented in:
+> - [GH-13](https://github.com/terraria-catalyst/Nitrate/pull/13)
+
+---
+
+The vanilla implementation of `TeleportPylonsSystem.IsPlayerNearAPylon` uses the `Player.IsTileTypeInInteractionRange` method to find if any pylon is nearby the player.
+
+### The Problem
+
+A single invocation of this method can (and usually does) perform over 14,000 tile lookups.
+
+### The Solution
+
+Instead of searching for nearby pylons from the player, search for the player from every pylon.
+The game limits the amount of pylons that can be placed[^1], putting an upper bound on the potential downside of this operation[^2].
+
+#### Results
+
+From testing, the original method would perform at an average of 0.5ms per invocation. The updated method reduces this to 0.005ms per invocation on average.
+
+[^1]: PENDING: What happens if a mod permits multiple types of the same pylon to be placed? Needs looking into.
+[^2]: Mods can, of course, add additional pylons, but this scales well enough and we will not end up with 14,000 pylons.
