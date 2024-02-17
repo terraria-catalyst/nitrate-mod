588732f1dcec95ca58f7c7c9b2a2a05848faaaf3
tomat <xxlennygamerxx@gmail.com>
2024-02-16 23:07:35 -0800

    Move to Catalyst Build Tools, Project Reorganization (#26)

---
+++ generate-symlinks.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const fs = require("fs");
+
+const dirs = ["Nitrate"];
+
+const sourceDirs = dirs.map((dir) => path.join(__dirname, "src", dir));
+const targetDirs = dirs.map((dir) => path.join(__dirname, "..", dir));
+
+for (let i = 0; i < sourceDirs.length; i++) {
+  const sourceDir = sourceDirs[i];
+  const targetDir = targetDirs[i];
+
+  if (!fs.existsSync(targetDir)) {
+    fs.symlinkSync(sourceDir, targetDir, "dir");
+  }
+}
