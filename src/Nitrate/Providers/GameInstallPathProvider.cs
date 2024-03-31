4f303aae3f289ff42777acf1678d6d2c63369a78
tomat <xxlennygamerxx@gmail.com>
2024-03-31 00:48:39 -0700

    I have basically not started, but commit these files so they don't sit unstaged.

---
+++ src/Nitrate/Providers/GameInstallPathProvider.cs
@@ -0,0 +1,20 @@
+﻿namespace TeamCatalyst.Nitrate.Providers;
+
+/// <summary>
+///     Provides the path to the game's installation directory.
+/// </summary>
+internal interface IGameInstallPathProvider {
+    /// <summary>
+    ///     Gets the path to the game's installation directory.
+    /// </summary>
+    string GetGameInstallPath();
+}
+
+/// <summary>
+///     Default implementation of <see cref="IGameInstallPathProvider"/>.
+/// </summary>
+internal sealed class GameInstallPathProvider : IGameInstallPathProvider {
+    string IGameInstallPathProvider.GetGameInstallPath() {
+        throw new System.NotImplementedException();
+    }
+}
