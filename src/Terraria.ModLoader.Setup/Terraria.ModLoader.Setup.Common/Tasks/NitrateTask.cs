3cd54e1e27bedd4d84085eba05b96e9eac20c03a
tomat <xxlennygamerxx@gmail.com>
2024-06-08 03:40:20 -0700

    Rewrite all defined types to be implicitly partial.

--- src/Terraria.ModLoader.Setup/Terraria.ModLoader.Setup.Common/Tasks/NitrateTask.cs
+++ src/Terraria.ModLoader.Setup/Terraria.ModLoader.Setup.Common/Tasks/NitrateTask.cs
@@ -1,6 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Terraria.ModLoader.Setup.Common.Tasks;
 
@@ -81,7 +86,107 @@ public sealed class NitrateTask : CompositeTask
 	
 	private sealed class MakeEveryTypePartialTask(CommonContext ctx, string sourceDirectory, string targetDirectory) : SetupOperation(ctx)
 	{
-		public override void Run() { }
+		private sealed class TypePartialRewriter : CSharpSyntaxRewriter
+		{
+			public override SyntaxNode? VisitClassDeclaration(ClassDeclarationSyntax node)
+			{
+				node = MakePartial(node);
+				return base.VisitClassDeclaration(node);
+			}
+			
+			public override SyntaxNode? VisitInterfaceDeclaration(InterfaceDeclarationSyntax node)
+			{
+				node = MakePartial(node);
+				return base.VisitInterfaceDeclaration(node);
+			}
+			
+			public override SyntaxNode? VisitRecordDeclaration(RecordDeclarationSyntax node)
+			{
+				node = MakePartial(node);
+				return base.VisitRecordDeclaration(node);
+			}
+			
+			public override SyntaxNode? VisitStructDeclaration(StructDeclarationSyntax node)
+			{
+				node = MakePartial(node);
+				return base.VisitStructDeclaration(node);
+			}
+			
+			private static ClassDeclarationSyntax MakePartial(ClassDeclarationSyntax node)
+			{
+				return node.Modifiers.Any(SyntaxKind.PartialKeyword) ? node : node.AddModifiers(SyntaxFactory.Token(SyntaxKind.PartialKeyword).WithTrailingTrivia(SyntaxFactory.Space));
+			}
+			
+			private static InterfaceDeclarationSyntax MakePartial(InterfaceDeclarationSyntax node)
+			{
+				return node.Modifiers.Any(SyntaxKind.PartialKeyword) ? node : node.AddModifiers(SyntaxFactory.Token(SyntaxKind.PartialKeyword).WithTrailingTrivia(SyntaxFactory.Space));
+			}
+			
+			private static RecordDeclarationSyntax MakePartial(RecordDeclarationSyntax node)
+			{
+				return node.Modifiers.Any(SyntaxKind.PartialKeyword) ? node : node.AddModifiers(SyntaxFactory.Token(SyntaxKind.PartialKeyword).WithTrailingTrivia(SyntaxFactory.Space));
+			}
+			
+			private static StructDeclarationSyntax MakePartial(StructDeclarationSyntax node)
+			{
+				return node.Modifiers.Any(SyntaxKind.PartialKeyword) ? node : node.AddModifiers(SyntaxFactory.Token(SyntaxKind.PartialKeyword).WithTrailingTrivia(SyntaxFactory.Space));
+			}
+		}
+		
+		public override void Run()
+		{
+			// For each given C# source file, we want to make every defined type
+			// partial (saves us patches later).
+			
+			var items = new List<WorkItem>();
+			
+			foreach (var (file, relPath) in EnumerateFiles(sourceDirectory))
+			{
+				var destination = Path.Combine(targetDirectory, relPath);
+				
+				// If not a C# source file, just copy and move on.
+				if (!relPath.EndsWith(".cs"))
+				{
+					copy(file, relPath, destination);
+					continue;
+				}
+				
+				items.Add(
+					new WorkItem(
+						"Partializing: " + relPath,
+						() =>
+						{
+							CreateParentDirectory(destination);
+							
+							if (File.Exists(destination))
+							{
+								File.SetAttributes(destination, FileAttributes.Normal);
+							}
+							
+							File.WriteAllText(destination, Partialize(File.ReadAllText(file), ctx.TaskInterface.CancellationToken));
+						}
+					)
+				);
+			}
+			
+			ExecuteParallel(items);
+			
+			return;
+			
+			void copy(string file, string relPath, string destination)
+			{
+				items.Add(new WorkItem("Copying: " + relPath, () => Copy(file, destination)));
+			}
+		}
+		
+		public static string Partialize(string source, CancellationToken cancellationToken)
+		{
+			var tree = CSharpSyntaxTree.ParseText(source, cancellationToken: cancellationToken);
+			var node = tree.GetRoot(cancellationToken);
+			node = new TypePartialRewriter().Visit(node);
+			
+			return node.ToFullString();
+		}
 	}
 	
 	private sealed class FormatWithDotnetFormatAndEditorConfigTask(CommonContext ctx, string sourceDirectory, string targetDirectory) : SetupOperation(ctx)
