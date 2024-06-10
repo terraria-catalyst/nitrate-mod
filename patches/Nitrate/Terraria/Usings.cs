48cbad78090a17a51d8c3747aea8140dd84380af
tomat <xxlennygamerxx@gmail.com>
2024-06-10 10:27:03 -0700

    Rewrite and Make Localization Extensible (#43)
    * Define softcoded languages.
    
    * Reimplement new language selection UI and full rewrites to the handling of languages to support extending them.
    
    * Fix formatting.
    
    * Prefer multiline comment.

---
+++ patches/Nitrate/Terraria/Usings.cs
@@ -0,0 +1,3 @@
+﻿// Give easy access to well-known language definitions for more natural code
+// since all related state is static.
+global using static Terraria.Nitrate.Localization.WellKnownLanguages;
