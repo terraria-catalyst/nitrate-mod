5c38ad3f1dfe39209897c9e8a4ad1b705e4eee4c
tomat <xxlennygamerxx@gmail.com>
2024-06-14 15:43:48 -0700

    Reimplement version branding API.

--- patches/Nitrate/Terraria/_Nitrate/Main.cs
+++ patches/Nitrate/Terraria/_Nitrate/Main.cs
@@ -1,4 +1,6 @@
 ﻿using Terraria.Nitrate.UI.States;
+using Terraria.Nitrate.VersionBranding;
+using Terraria.Nitrate.VersionBranding.UI;
 
 namespace Terraria;
 
@@ -6,4 +8,6 @@ partial class Main
 {
 	private static UILanguageSelectMenu languageSelectMenu_InitialLanguageSelect = new(true);
 	private static UILanguageSelectMenu languageSelectMenu = new(false);
+
+	private static readonly VersionBrandingRenderer version_branding_renderer = new(WellKnownVersionBrandings.VANILLA, WellKnownVersionBrandings.TML, WellKnownVersionBrandings.NITRATE);
 }
