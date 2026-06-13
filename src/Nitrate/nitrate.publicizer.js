d221151cb3973576787072fc8534212761141a9a
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-06-13 09:24:09 -0500

    build: switch to tml-build

--- src/Nitrate/nitrate.publicizer.js
+++
@@ -1,6 +0,0 @@
-﻿import {createPublicizer} from "publicizer";
-
-export const publicizer = createPublicizer("Nitrate");
-
-publicizer.createAssembly("tModLoader").publicizeAll();
-publicizer.createAssembly("FNA").publicizeAll();
